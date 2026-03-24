e8436ef188fa0ff7f25b236ce400713103ba3fc0
Pirijadis <antcov0101@gmai.com>
2026-03-24 17:02:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IRproject1PIS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
